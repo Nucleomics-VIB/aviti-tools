e2a465c0b9f6822506617585e70644e6103ea1b1
Stephane Plaisance <stephane.plaisance@vib.be>
2023-01-19 16:00:50 +0100

    Update bases2fastq_docker.sh

--- bases2fastq_docker.sh
+++ bases2fastq_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# bases2fastq_docker.sh
+# bases2fastq_adept_docker.sh
 # basecall Aviti data from a completed run folder
 # run the command from the folder enclosing the run folder !!
 # run from docker as uid:gid to avoid outputfolder being owned by root
@@ -8,20 +8,24 @@
 # Stéphane Plaisance - VIB-NC June-06-2018 v1.0
 # visit our Git: https://github.com/Nucleomics-VIB
 
-version="1.0, 2023_04_18"
+version="1.01, 2023_04_18"
 
 usage='# Usage: bases2fastq_docker.sh
-# -r <path to run folder (in the current path)> 
+# -i <path to run folder (in the current path)> 
 # -o <path to output folder (in the current path)>
+# -m <path to custom RunManifest.csv (by default RunManifest.csv in the run folder)>
 # optional -t <threads> (default 8)>
+# optional -l <log level (default INFO, DEBUG for more)>
 # script version '${version}'
 # [-h for this help]'
 
-while getopts "r:o:t:h" opt; do
+while getopts "i:o:m:t:l:h" opt; do
   case $opt in
-    r) runfolder=${OPTARG} ;;
+    i) runfolder=${OPTARG} ;;
     o) outfolder=${OPTARG} ;;
+    m) manifest=${OPTARG} ;;
     t) threads=${OPTARG} ;;
+    l) loglevel=${OPTARG} ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
@@ -30,7 +34,7 @@ done
 
 # get the docker (run once)
 # docker pull elembio/bases2fastq
-# elembio/bases2fastq              latest              ab8ff63330af   3 months ago    1.02GB
+# elembio/bases2fastq latest ab8ff63330af 3 months ago 1.02GB
 
 # check if a docker image is available (command below returns its ID)
 dockerimg="elembio/bases2fastq"
@@ -40,7 +44,7 @@ if [[ "$(docker images -q ${dockerimg}:${dockertag} 2> /dev/null)" == "" ]]; the
     exit 1
 fi
 
-# check if arguments were provided
+# check if required arguments were provided
 if [ -z "${runfolder+x}" ] || [ -z "${outfolder+x}" ]
 then
    echo "# please provide mandatory arguments -r and -o!"
@@ -48,31 +52,37 @@ then
    exit 1
 fi
 
-# check run folder exists
+# check if run folder exists
 if [ ! -d "${runfolder}" ]; then
 	echo "${runfolder} folder not found!"
 	exit 1
 fi
 
-# check thet run folder is complete and contains RunUploaded.json
+# check if run folder is complete and contains RunUploaded.json
 if [ -z "${runfolder}/RunUploaded.json" ]; then
     echo "# the run folder does not contain 'RunUploaded.json'"
     echo "# it might not be fully transferred, please check"
     exit 1
 fi
 
-# default num threads
+# default or custom num threads
 nthr=${threads:-8}
 
-# or simplified to
+# default or increased logging
+loglv=${loglevel:-"INFO"}
+
+# default or custom manifest
+manifest=${manifest:-"${runfolder}/RunManifest.csv"}
+
+# calling docker
 docker run \
   --rm \
   --user "$(id -u):$(id -g)" \
   -v $PWD:/workdir \
   elembio/bases2fastq:latest bases2fastq \
     -p ${nthr} \
-    --settings 'R1AdapterTrim,FALSE' \
-    --settings 'R2AdapterTrim,TRUE' \
+    --run-manifest /workdir/${manifest} \
     --legacy-fastq \
+    --log-level ${loglv} \
     /workdir/${runfolder} \
     /workdir/${outfolder}
