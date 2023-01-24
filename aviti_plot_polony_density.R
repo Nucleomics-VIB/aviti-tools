40be0fe80221a48597a1b836e766a8de326d3719
Stephane Plaisance <stephane.plaisance@vib.be>
2023-01-24 11:10:56 +0100

    Update aviti_plot_polony_density.R

--- aviti_plot_polony_density.R
+++ aviti_plot_polony_density.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
 
 # script: aviti_plot_polony_density.R
-# Stéphane Plaisance - VIB-Nucleomics Core - 2023-01-23 v1.0
+# Stéphane Plaisance - VIB-Nucleomics Core - 2023-01-23 v1.01
 
-library("ComplexHeatmap")
-library("ggplot2")
+suppressPackageStartupMessages(library("ComplexHeatmap"))
+suppressPackageStartupMessages(library("grid"))
 
 # produce the two input files with:
 # aviti_count_tiles.sh 1 > tiles_lane1.txt
@@ -12,34 +12,39 @@ library("ggplot2")
 
 # setwd("/data/analyses/aviti_bases2fastq/20230117_AV224503_4353-4354_20230117_AVITI_A_fastq_sp_notrim")
 
+# load Lane1 data
 lane1 <- read.table("tiles_lane1.txt", quote="\"", comment.char="")
 colnames(lane1) <- c("count", "tile")
 
-l1.data <- matrix(lane1$count, 53, byrow=TRUE)
+# add 3 NA where no data present
+l1.data <- matrix(c(NA, lane1$count[c(1:3)], NA, lane1$count[-c(1:3)], NA), 43, byrow=TRUE)
 
 hm1 <- ComplexHeatmap::Heatmap(l1.data, 
-               name="lane1",
-               cluster_rows = FALSE,
-               cluster_columns = FALSE)
+                               name="lane1",
+                               cluster_rows = FALSE,
+                               cluster_columns = FALSE)
 
+# load Lane1 data
 lane2 <- read.table("tiles_lane2.txt", quote="\"", comment.char="")
 colnames(lane2) <- c("count", "tile")
 
-l2.data <- matrix(lane2$count, 53, byrow=TRUE)
+# add 3 NA where no data present
+l2.data <- matrix(c(NA, lane2$count[c(1:3)], NA, lane2$count[-c(1:3)], NA), 43, byrow=TRUE)
 
 hm2 <- ComplexHeatmap::Heatmap(l2.data, 
                                name="lane2",
                                cluster_rows = FALSE,
                                cluster_columns = FALSE)
 
+# assemble heatmaps and produce plot
 ht_list = hm1 + hm2
 
 pdf(file="aviti_loading_density.pdf",width = 5, height = 10, bg = "white")
 
-draw(ht_list, 
+ComplexHeatmap::draw(ht_list, 
      row_title = "Aviti polonies", 
      row_title_gp = gpar(col = "red"),
      column_title = "Lanes", 
      column_title_side = "bottom")
 
-dev.off()
+null <- dev.off()
